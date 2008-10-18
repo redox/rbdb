65d05c10f4053ca16366f812212690f0fd6e0642
Sylvain Utard - redox <sylvain.utard@gmail.com>
2008-10-18 23:01:04 +0200

    * Fix unit tests

--- test/functional/tables_controller_test.rb
+++ test/functional/tables_controller_test.rb
@@ -2,20 +2,5 @@ require File.dirname(__FILE__) + '/../test_helper'
 
 class TablesControllerTest < ActionController::TestCase
   
-  setup do
-    ActiveRecord::Base.connection.execute "create database sourceforge_test"
-    ActiveRecord::Base.connection.execute "use sourceforge_test"
-    ActiveRecord::Base.connection.execute IO.read("#{Rails.root}/test/fixtures/sourceforge_database_short.sql")
-  end
-  
-  teardown do
-    ActiveRecord::Base.connection.execute "drop database sourceforge_test"
-  end
-  
-  should "have 7 tables" do
-    get :controller => 'tables', :id => 'sourceforge'
-    assert_response :success
-  end
-  
     
 end
