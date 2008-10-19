5a68f4b4b7b104fb6ff23d6312a1c3f360d04f94
Sylvain Utard - redox <sylvain.utard@gmail.com>
2008-10-19 15:58:03 +0200

    * Clicking on a foreign value does no redirect to the table structure anymore but on the foreign record

--- app/controllers/rows_controller.rb
+++ app/controllers/rows_controller.rb
@@ -3,6 +3,13 @@ class RowsController < ApplicationController
   before_filter :select_db
   before_filter :select_table
   
+  def show
+    @row = WillPaginate::Collection.create(1, 1) do |pager|
+      pager.replace [@table.ar_class.find(params[:id])]
+      pager.total_entries = 1
+    end
+  end
+  
   def update
     row = @table.ar_class.find params[:id]
     if row.update_attributes params[:row]
