11c518b79d67faee2a291a47c8f0c0d135b2c085
Sylvain Utard - redox <sylvain.utard@gmail.com>
2008-10-19 22:11:31 +0200

    * Add environment

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 ActionController::Routing::Routes.draw do |map|
+  map.resources :environments
+
   map.resources :databs do |datab|
     datab.resources :tables do |table|
       table.resources :rows
