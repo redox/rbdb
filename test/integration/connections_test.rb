872fa8763f16311b9e88915382036f1ab43ad0e2
Ivan <ivan@ivans-imac.local>
2008-11-11 16:05:37 +0100

    a failing test for the concurrent sessions bug

---
+++ test/integration/connections_test.rb
@@ -0,0 +1,19 @@
+require File.dirname(__FILE__) + '/../test_helper'
+
+class ConnectionsTest < ActionController::IntegrationTest
+
+  def test_one_valid_user_and_one_fake
+    conf = YAML.load_file("#{Rails.root}/config/database.yml")['test']
+    valid_user = open_session
+    valid_user.post '/accounts/login', :username => conf[:username],
+      :password => conf[:password]
+    valid_user.assert_response :redirect
+    valid_user.assert_redirected_to '/databs'
+    fake_user = open_session
+    fake_user.post '/accounts/login', :username => 'phony', :password => 'so fake'
+    fake_user.assert_response :success
+    valid_user.get '/databs'
+    valid_user.assert_response :success
+  end
+  
+end
