4b5d6c7c5555f9397cbfc1e1175ee9e03a381bc6
Sylvain Utard - redox <sylvain.utard@gmail.com>
2008-12-16 00:33:03 +0100

    * DateTime/Date can now be edited in place (should be improved with a    JS edit-in-place integration)

--- app/helpers/tables_helper.rb
+++ app/helpers/tables_helper.rb
@@ -8,7 +8,32 @@ module TablesHelper
   end
 
   def editable_field row, column, content
-    edit_in_place row, column.name, :url => datab_table_row_path(@datab, @table, row[:id]), :content => content, :dom_id => "row_#{row[:id]}"
+    case column.type
+    when :datetime, :time, :date
+      _erbout = ''
+      field_name = "field_#{column.name}_#{row.id}"
+      form_name = "edit_#{column.name}_#{row.id}"
+      content_tag :span, :id => field_name,
+        :onclick => "$('#{form_name}').show();$('#{field_name}').hide();" do
+        _erbout.concat date_ago(content)
+      end
+      remote_form_for(row, :url => datab_table_row_path(@datab, @table, row),
+        :html => {:style => 'display: none', :id => form_name},
+        :complete => "$('#{form_name}').hide();$('#{field_name}').show();$('#{field_name}').update(request.responseText.evalJSON()['#{column.name}'])") do
+        fields_for 'row', row do |f|
+          _erbout.concat f.calendar_date_select(column.name)
+          _erbout.concat f.submit('Save')
+          cancel_link = link_to_function('cancel') do |page|
+            page[form_name].hide
+            page[field_name].show
+          end
+          _erbout.concat cancel_link
+        end
+      end
+    else
+      edit_in_place row, column.name, :url => datab_table_row_path(@datab, @table, row[:id]),
+        :content => content, :dom_id => "row_#{row[:id]}"
+    end
   end
   
   def number_delimited num
